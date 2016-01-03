2a90c3de7e680b51584a00f8dcc83cb3c6b696c5
Filip-Konkowski <fkonkowski@gmail.com>
2016-01-03 15:29:46 +0100

    new bundle Jobeet, Entities, Fixtures

---
+++ src/JobeetBundle/Entity/Category.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping;
+
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $jobs;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $category_affiliates;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->jobs = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->category_affiliates = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add job
+     *
+     * @param \JobeetBundle\Entity\Job $job
+     *
+     * @return Category
+     */
+    public function addJob(\JobeetBundle\Entity\Job $job)
+    {
+        $this->jobs[] = $job;
+
+        return $this;
+    }
+
+    /**
+     * Remove job
+     *
+     * @param \JobeetBundle\Entity\Job $job
+     */
+    public function removeJob(\JobeetBundle\Entity\Job $job)
+    {
+        $this->jobs->removeElement($job);
+    }
+
+    /**
+     * Get jobs
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getJobs()
+    {
+        return $this->jobs;
+    }
+
+    /**
+     * Add categoryAffiliate
+     *
+     * @param \JobeetBundle\Entity\CategoryAffiliate $categoryAffiliate
+     *
+     * @return Category
+     */
+    public function addCategoryAffiliate(\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliate)
+    {
+        $this->category_affiliates[] = $categoryAffiliate;
+
+        return $this;
+    }
+
+    /**
+     * Remove categoryAffiliate
+     *
+     * @param \JobeetBundle\Entity\CategoryAffiliate $categoryAffiliate
+     */
+    public function removeCategoryAffiliate(\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliate)
+    {
+        $this->category_affiliates->removeElement($categoryAffiliate);
+    }
+
+    /**
+     * Get categoryAffiliates
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCategoryAffiliates()
+    {
+        return $this->category_affiliates;
+    }
+}
--- src/JobeetBundle/JobeetBundle.php
+++ src/JobeetBundle/JobeetBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace JobeetBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class JobeetBundle extends Bundle
 {
 }
